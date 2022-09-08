d8fd785269b29c2f8f7bf5ef2acea91555a77700
Iván Viemoes Cuevas <111234017+IvanViemoesCuevas@users.noreply.github.com>
2022-09-08 11:23:50 +0200

    Update SimpleStorage.sol

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -7,21 +7,26 @@ contract SimpleStorage {
     uint256 favoriteNumber;
 
     struct People {
+        //Creates a struct of people with their favorite number, and name
         uint256 favoriteNumber;
         string name;
     }
 
+    //creates an array mapping where a name points to a number
     People[] public people;
     mapping(string => uint256) public nameToFavoriteNumber;
 
+    //Stores the number as a uint256 variable named favoriteNumber
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
     }
 
+    //retrieves the saved favoriteNumber
     function retrieve() public view returns(uint256) {
         return favoriteNumber;
     }
 
+    //Saves the name and number in the people struct, and in the array mapping
     function addPerson(string memory _name, uint256 _favoriteNumber) public {
         people.push(People({favoriteNumber: _favoriteNumber, name: _name}));
         nameToFavoriteNumber[_name] = _favoriteNumber;
